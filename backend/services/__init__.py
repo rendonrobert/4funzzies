62766aae9c01681b8e6ac4fbc18037caa7121028
Robert <148299490+rendonrobert@users.noreply.github.com>
2025-02-22 20:22:15 -0800

    Delete backend confusion

--- backend/services/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Services package for song identification and lyrics retrieval.
-Contains ShazamService and LyricsService modules.
-"""
-
-from .shazam_service import ShazamService
-from .lyrics_service import LyricsService
-
-__all__ = ['ShazamService', 'LyricsService'] 
